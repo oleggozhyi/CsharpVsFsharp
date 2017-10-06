76d556063f8ee057758c2a09548f45b493daa7c3
Oleg Gozhyi <oleg.gozhyi@outlook.com>
2017-10-06 09:33:26 +0300

    m

--- src/Fsharp/StringToInt.fs
+++ src/Fsharp/StringToInt.fs
@@ -1,5 +1,7 @@
 ﻿module StringToInt
 
+open ComputationExpressions
+
 // no mutation
 // no statements, only expressions
 // no control flow, only data flow
@@ -18,18 +20,17 @@ let charToDigit (c: char) =
 //      |> charsToIntRec 0
 
 type MaybeBuilder() = 
-    member __.Bind(f,x) = Option.bind
-    member __.Return(x) = Some
+    member __.Bind(x, f) = Option.bind f x
+    member __.Return(x) = Some x
 
 let maybe = new MaybeBuilder();
      
 let strToIntFold s =
-    let reducer acc ch = 
-        match acc with 
-        | None -> None
-        | Some accValue -> match charToDigit ch with 
-                | None -> None
-                | Some i -> accValue * 10 + i |> Some 
+    let reducer (acc:int option)  (ch:char) = maybe {
+        let! accValue = acc
+        let! digit = charToDigit ch
+        return accValue * 10 + digit
+      }
     s |> Seq.fold reducer (Some 0)
         
 
@@ -42,15 +43,30 @@ let strToUnsignedInt s =
                 | Some i -> accValue * 10 + i |> Some 
     s |> Seq.fold reducer (Some 0)
         
-let getSignAndUnsignedString (s: string) = 
-    if s.Length > 1 && s.Chars(0) = '-' then -1, (s.Substring (1)) 
-    else 1, s
-
+let getSignAndUnsignedString (s: string) = match s with
+    | empty when empty.Length =0 -> None
+    | s when s.Chars(0) = '-' -> (-1, s.Substring(1)) |> Some
+    | s when System.Char.IsDigit(s.Chars(0)) -> (1,s) |> Some
 
 let strToSignedInt s = 
-    let sign, unsignedString = getSignAndUnsignedString s
-    sign * (strToUnsignedInt unsignedString)
-
+    match getSignAndUnsignedString s with
+    | None -> None
+    | Some (sign, unsignedString) -> match strToUnsignedInt unsignedString with
+            | None -> None
+            | Some result -> result * sign |> Some
+        
+    
+let bind (x:int option)  (f:int-> int option) = match x with 
+    | None -> None
+    | Some i -> f i
+let (>>=) = bind
 
     
 
+let strToUnsignedInt1 s =
+    let reducer acc ch = 
+        acc >>= fun accValue -> 
+            charToDigit ch >>= fun i -> 
+                accValue * 10 + i |> Some            
+     
+    s |> Seq.fold reducer (Some 0)
